bf4af8a6bf647e89fad0eddcdd95f03b9b6eb1c9
Gerard Wagener <gerard.wagener@circl.lu>
2023-12-28 15:45:11 +0100

    initial version

---
+++ typo3temp/assets/compressed/merged-9a69b281ac4758956c03a7e874da1f05-ed1878d4bfe1b09c97764205455d629dcb8b.js
@@ -0,0 +1,7 @@
+
+/*!
+ * LIST Project Template v1.0.0 (https://www.list.lu/)
+ * Copyright 2017-2020 Emmanuel DENOYELLE
+ * Licensed under the GPL-2.0-or-later license
+ */
+console.log("WE LOVE TYPO3");
